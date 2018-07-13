b74553bccd47139d982fee5be3cad37d95a833ef
Rohith Singirikonda <singirikonda.rohith@gmail.com>
2018-07-13 11:03:47 +0530

    Test Base class Ready

---
+++ src/main/java/com/HRM/util/TestUtil.java
@@ -0,0 +1,31 @@
+package com.HRM.util;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import com.HRM.base.TestBase;
+
+public class TestUtil extends TestBase{
+	
+		
+		public static int Implicit_wait = 20;
+		public static int PageLoad_Timeout=30;
+	
+	
+	
+	
+	
+	
+	public static void takeScreenshotAtEndOfTest() throws IOException{
+		
+		File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(srcFile, new File(System.getProperty("user.dir")+"/screenshots/"+System.currentTimeMillis()+".png"));
+		
+	}
+	
+	
+}
