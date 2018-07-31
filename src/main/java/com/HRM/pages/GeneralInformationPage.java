8d798dc91a0eafde4bd216d153d60f6b79a57305
Rohith Singirikonda <singirikonda.rohith@gmail.com>
2018-07-31 12:56:12 +0530

    General Info Page Working

--- src/main/java/com/HRM/pages/GeneralInformationPage.java
+++ src/main/java/com/HRM/pages/GeneralInformationPage.java
@@ -1,8 +1,68 @@
 package com.HRM.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
 import com.HRM.base.TestBase;
 
 public class GeneralInformationPage extends TestBase {
 
+	@FindBy(id="btnSaveGenInfo")
+	WebElement saveBtn;
+
+	
+	public GeneralInformationPage(){
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+	public String GenInfoPageHeader(){
+		String page_header = driver.findElement(By.id("genInfoHeading")).getText();
+		return page_header;
+	}
+
+	
+	public String GenInfoPageValidation(String OrgName, String TaxId, String RegNo, String Phone, String Fax, String Email,
+							String Address1, String Address2, String City, String State, String Zipcode, String Country, String Note){
+		
+		String message = null;
+		WebElement org_name = driver.findElement(By.id("organization_name"));
+		
+		if (!org_name.isEnabled()) {
+		
+			saveBtn.click();
+			
+			org_name.clear();
+			org_name.sendKeys(OrgName);
+			driver.findElement(By.id("organization_taxId")).sendKeys(TaxId);
+			driver.findElement(By.id("organization_registraionNumber")).sendKeys(RegNo);
+			driver.findElement(By.id("organization_phone")).sendKeys(Phone);
+			driver.findElement(By.id("organization_fax")).sendKeys(Fax);
+			driver.findElement(By.id("organization_email")).sendKeys(Email);
+			driver.findElement(By.id("organization_street1")).sendKeys(Address1);
+			driver.findElement(By.id("organization_street2")).sendKeys(Address2);
+			driver.findElement(By.id("organization_city")).sendKeys(City);
+			driver.findElement(By.id("organization_province")).sendKeys(State);
+			driver.findElement(By.id("organization_zipCode")).sendKeys(Zipcode);
+			//Select select = new Select(driver.findElement(By.name("organization[country]")));
+			//select.selectByVisibleText(Country);
+			driver.findElement(By.id("organization_note")).sendKeys(OrgName);
+			
+			
+			if (!org_name.equals(null) || !org_name.equals("")) {
+				message = driver.findElement(By.xpath("//*[@id='frmGenInfo']//span[@for='organization_name']")).getText();
+			} else {
+				message = driver.findElement(By.xpath("//div[@class='message success fadable']")).getText();
+			}
+		}
+		
+		return message;
+		
+	}
+	
+	
 	
 }
---
+++ src/test/java/com/HRM/testcases/GeneralInformationPageTest.java
@@ -0,0 +1,88 @@
+package com.HRM.testcases;
+
+import org.junit.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.HRM.base.TestBase;
+import com.HRM.pages.AdminHomePage;
+import com.HRM.pages.AdminModulePage;
+import com.HRM.pages.GeneralInformationPage;
+import com.HRM.pages.LoginPage;
+import com.HRM.util.TestUtil;
+
+public class GeneralInformationPageTest extends TestBase {
+	
+	public static String actual_message;
+	String generalInfo_sheet = "generalInfo";
+
+
+	LoginPage loginPage;
+	AdminHomePage admin_HomePage;
+	AdminModulePage admin_ModulePage;
+	GeneralInformationPage generalInformationPage;
+	TestUtil testUtil;
+	
+	public GeneralInformationPageTest() {
+		super();
+	}
+	
+	
+
+	@BeforeMethod
+	public void setUp() {
+		init();
+		loginPage = new LoginPage();
+		admin_HomePage = new AdminHomePage();
+		admin_ModulePage = new AdminModulePage();
+		generalInformationPage =  new GeneralInformationPage();
+		testUtil = new TestUtil();
+		admin_HomePage = loginPage.AdminLogin(prop.getProperty("admin_username"), prop.getProperty("admin_password"));
+		admin_ModulePage = admin_HomePage.clickOnAdminPageLink();
+		generalInformationPage = admin_ModulePage.ClickonGeneralInfoMenu();
+	}
+	
+	
+	@Test(priority=1, enabled=false)
+	public void VerifyGenInfoPageHeaderTest(){
+		actual_message = generalInformationPage.GenInfoPageHeader();
+		Assert.assertEquals("General Information", actual_message);
+	}
+	
+	
+	@DataProvider(name = "GenInfoDetails")
+	public Object[][] getNewToursTestData(){
+		Object data[][] = TestUtil.getTestData(generalInfo_sheet);
+		return data;
+	}
+	
+	
+	@Test(dataProvider="GenInfoDetails", priority=2, enabled=true)
+	public void VerifyGenInfoPageValidationTest(String OrgName, String TaxId, String RegNo, String Phone, String Fax, String Email,
+			String Address1, String Address2, String City, String State, String Zipcode, String Country, String Note){
+		
+		String message = generalInformationPage.GenInfoPageValidation(OrgName, TaxId, RegNo, Phone, Fax, Email, Address1, Address2, City, State, Zipcode, Country, Note);
+		
+		if (message.contains("Required")) {
+			System.out.println(message);
+		} else {
+			System.out.println(message);
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.close();
+	}
+	
+	
+	
+}
